77c95bf3a6a350835b3542d7bdf40f4512c11fae
devdgg <devdgg@hanmail.net>
2016-03-16 01:38:19 +0900

    [v0.0.1] add script(debug)

---
+++ Assets/Gna/Scripts/DrawGizmos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawGizmos : MonoBehaviour {
+#if UNITY_EDITOR
+    private Texture2D blackBackground;
+
+    // Use this for initialization
+    void Start () {
+
+        blackBackground = new Texture2D(1, 1);
+        blackBackground.SetPixel(0, 0, new Color(0.0f, 0.0f, 0.0f, 0.8f));
+        blackBackground.Apply();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   
+    private void OnDrawGizmos()
+    {
+        UnityEditor.Handles.BeginGUI();
+        GUI.color = Color.white;
+        string text = " " + gameObject.name + " ";
+        var view = UnityEditor.SceneView.currentDrawingSceneView;
+        Vector3 screenPos = view.camera.WorldToScreenPoint(gameObject.transform.position);
+        Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
+        GUI.skin.box.normal.background = blackBackground;
+        Rect rect = new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y);
+        GUI.Box(rect, GUIContent.none);
+        GUI.Label(rect, text);
+        UnityEditor.Handles.EndGUI();
+    }
+#endif
+}
---
+++ Assets/Gna/Scripts/Log/MouseCoord.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GUIText))]
+public class MouseCoord : MonoBehaviour {
+
+    GUIText gui;
+
+	// Use this for initialization
+	void Start () {
+
+        gui = GetComponent<GUIText>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if( gui != null)
+        {
+            Vector3 pos = Input.mousePosition;
+            gui.text = "mouse position: " + pos.x + ", " + pos.y;
+        }
+	}
+}
